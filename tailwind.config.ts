a42385101a077c25b6c70f6b67ff3628497bc1c0
Dmitriy Malakhov <mitia2022@gmail.com>
2023-05-02 15:35:12 +0700

    style(tailwind.config.ts): fix formatting and add eslint-disable comment feat(tailwind.config.ts): add tailwindcss-animate plugin to plugins list

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import { type Config } from "tailwindcss";
-import {fontFamily} from 'tailwindcss/defaultTheme'
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
@@ -57,12 +57,12 @@ export default {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: '0' },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: '0' },
+          to: { height: "0" },
         },
       },
       animation: {
@@ -71,5 +71,7 @@ export default {
       },
     },
   },
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   plugins: [import("tailwindcss-animate")],
 } satisfies Config;
